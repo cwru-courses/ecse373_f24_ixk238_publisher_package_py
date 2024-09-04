09a0bd5f60b1d40ffb6fdfea73896d80bb86cffd
ilke-kas <ixk238@case.edu>
2024-09-04 14:20:53 -0400

    topic and node name is updates

--- scripts/publisher_code.py
+++ scripts/publisher_code.py
@@ -6,6 +6,7 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher('publisher_topic', String, queue_size=10)
     rospy.init_node('publisher_node', anonymous=True)
+    
     rate = rospy.Rate(1) # 1hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
